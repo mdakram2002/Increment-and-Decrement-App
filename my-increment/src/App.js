554d4b557eb6b9662685a7c4dbbd63d43ad87297
Md <mdakram12022002@gmail.com>
2024-09-24 00:20:53 +0530

    some changes in App.js and Adding tailwindcss

--- my-increment/src/App.js
+++ my-increment/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  function decreaseHandler() {
+    if (count > 0) {
+      setCount(count - 1);
+    };
+  }
+  function increaseHandler() {
+    setCount(count + 1);
+  }
+  function resetHandler() {
+    setCount(0);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Increment & Decrement</h1>
+        <div className="calculate">
+          <button onClick={decreaseHandler}>
+            +
+          </button>
+          <div className="count">{count}</div>
+          <button onClick={increaseHandler}>
+            -
+          </button>
+        </div>
+        <button className="reset" onClick={resetHandler}>Reset</button>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
